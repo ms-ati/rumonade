0ba6358a4210a3663b37f0361976a85bbe692183
Marc Siegel <marc@usainnov.com>
2012-04-29 15:48:51 -0400

    updated documentation for Monad

--- lib/rumonade/monad.rb
+++ lib/rumonade/monad.rb
@@ -1,13 +1,18 @@
 module Rumonade
-  # Mix-in for common monad functionality dependent on implementation of monadic methods unit and bind
+  # Mix-in for common monad functionality dependent on implementation of monadic methods +unit+ and +bind+
   #
   # Notes:
-  # * classes should include this module AFTER defining the monadic methods unit and bind
+  # * Classes should include this module AFTER defining the monadic methods +unit+ and +bind+
+  # * When +Monad+ is mixed into a class, if the class already contains methods in
+  #   {METHODS_TO_REPLACE}, they will be renamed to add the suffix +_without_monad+,
+  #   and replaced with the method defined here which has the suffix +_with_monad+
   #
   module Monad
-    METHODS_TO_REPLACE = [:flat_map, :flatten] # :nodoc:
+    # Names of methods to replace when mixed into a class
+    METHODS_TO_REPLACE = [:flat_map, :flatten]
 
-    def self.included(base) # :nodoc:
+    # When mixed into a class, this callback is executed
+    def self.included(base)
       base.class_eval do
         # optimization: replace flat_map with an alias for bind, as they are identical
         alias_method :flat_map_with_monad, :bind
@@ -33,14 +38,19 @@ module Rumonade
     end
 
     # Returns the results of applying the given function to each element in this monad
+    #
+    # NOTE: aliased as +flat_map+ when +Monad+ is mixed into a class
     def flat_map_with_monad(lam = nil, &blk)
       bind(lam || blk)
     end
 
     # Returns a monad whose elements are the ultimate (non-monadic) values contained in all nested monads
     #
-    #    [1] == [[Some(Some(1)), Some(Some(None))], [None]].flatten
-    #    => true
+    # NOTE: aliased as +flatten+ when +Monad+ is mixed into a class
+    #
+    # @example
+    #   [Some(Some(1)), Some(Some(None))], [None]].flatten
+    #   #=> [1]
     #
     def flatten_with_monad
       bind { |x| x.is_a?(Monad) ? x.flatten_with_monad : self.class.unit(x) }
@@ -56,6 +66,16 @@ module Rumonade
     #
     # This method is equivalent to the Scala flatten call (single-level flattening), whereas #flatten is in keeping
     # with the native Ruby flatten calls (multiple-level flattening).
+    #
+    # @example
+    #   [Some(Some(1)), Some(Some(None))], [None]].shallow_flatten
+    #   #=> [Some(Some(1)), Some(Some(None)), None]
+    #   [Some(Some(1)), Some(Some(None)), None].shallow_flatten
+    #   #=> [Some(1), Some(None)]
+    #   [Some(1), Some(None)].shallow_flatten
+    #   #=> [1, None]
+    #   [1, None].shallow_flatten
+    #   #=> [1]
     #
     def shallow_flatten
       bind { |x| x.is_a?(Monad) ? x : self.class.unit(x) }
