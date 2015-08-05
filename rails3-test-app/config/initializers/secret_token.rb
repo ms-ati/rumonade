076a2dc7dc0525035c75374ee2f5a183d4bd4fb6
Marc Siegel <marc@usainnov.com>
2015-08-05 11:30:20 -0400

    Add rail3 test app

---
+++ rails3-test-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3TestApp::Application.config.secret_token = '0443e8524bef1830e4b92ec51d46b6bd56e414ba775b9f1812fe3a8a15ac43e2a599e3db31e64eeca279523a28b7e2797c0ebec5dcd5b23ef8a48e78ea1a2d20'
