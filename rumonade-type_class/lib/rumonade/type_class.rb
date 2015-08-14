e9b2df257c6737a9157ea52caf70bfa97acb6cb8
Marc Siegel <marc@usainnov.com>
2015-08-14 12:34:11 -0500

    Hack a bit

--- rumonade-type_class/lib/rumonade/type_class.rb
+++ rumonade-type_class/lib/rumonade/type_class.rb
@@ -1,83 +1,85 @@
-class TypeClass
-  module Finders
-  end
-
-  class << self
-    def [](klass)
-      all[self][klass] || if klass.superclass && klass.superclass != Object
-                            self[klass.superclass]
-                          else
-                            raise("No type class instance found for #{self}[#{klass}]")
-                          end
+module Rumonade
+  class TypeClass
+    module Finders
     end
 
-    def []=(klass, impl)
-      all[self][klass] = impl.new
-      type_class = self
-      Finders.module_eval do
-        define_method(type_class.to_s.to_sym) do |object|
-          type_class[object.class].proxy(object)
+    class << self
+      def [](klass)
+        all[self][klass] || if klass.superclass && klass.superclass != Object
+                              self[klass.superclass]
+                            else
+                              raise("No type class instance found for #{self}[#{klass}]")
+                            end
+      end
+
+      def []=(klass, impl)
+        all[self][klass] = impl.new
+        type_class = self
+        type_class_name = type_class.to_s.split('::')[-1].to_sym
+        Finders.module_eval do
+          define_method(type_class_name) do |object|
+            type_class[object.class].proxy(object)
+          end
+          module_function type_class_name
         end
-        module_function type_class.to_s.to_sym
+      end
+
+      def all
+        @all ||= Hash.new({})
+      end
+
+      def inherited(subclass)
+        puts "inherited: #{subclass} from #{self}"
+        # Finders.module_eval do
+        #   define_method(subclass.to_s.to_sym) do |object|
+        #     subclass[object.class].proxy(object)
+        #   end
+        #   module_function subclass.to_s.to_sym
+        # end
       end
     end
 
-    def all
-      @all ||= Hash.new({})
+    def proxy(object)
+      if self.class.const_defined? :SyntaxProxy
+        self.class::SyntaxProxy.new(self, object)
+      else
+        object
+      end
     end
 
-    def inherited(subclass)
-      puts "inherited: #{subclass} from #{self}"
-      # Finders.module_eval do
-      #   define_method(subclass.to_s.to_sym) do |object|
-      #     subclass[object.class].proxy(object)
-      #   end
-      #   module_function subclass.to_s.to_sym
-      # end
-    end
-  end
-
-  def proxy(object)
-    if self.class.const_defined? :SyntaxProxy
-      self.class::SyntaxProxy.new(self, object)
-    else
-      object
+    class BaseSyntaxProxy
+      def initialize(impl, object)
+        @impl = impl
+        @object = object
+      end
     end
   end
 
-  class BaseSyntaxProxy
-    def initialize(impl, object)
-      @impl = impl
-      @object = object
+  class Semigroup < TypeClass
+    def append(a, b)
+    end
+  end
+
+  class SemigroupPlus < Semigroup
+    def append(a, b)
+      a + b
+    end
+  end
+
+  class SemigroupTimes < Semigroup
+    def append(a, b)
+      a * b
+    end
+
+    class SyntaxProxy < TypeClass::BaseSyntaxProxy
+      def append(other)
+        @impl.append(@object, other)
+      end
     end
   end
 end
 
-class Semigroup < TypeClass
-  def append(a, b)
-  end
-end
-
-class SemigroupPlus < Semigroup
-  def append(a, b)
-    a + b
-  end
-end
-
-class SemigroupTimes < Semigroup
-  def append(a, b)
-    a * b
-  end
-
-  class SyntaxProxy < TypeClass::BaseSyntaxProxy
-    def append(other)
-      @impl.append(@object, other)
-    end
-
-    def ||()
-  end
-end
-
+include Rumonade
 
 Semigroup[String] = SemigroupPlus
 Semigroup[Numeric] = SemigroupTimes
